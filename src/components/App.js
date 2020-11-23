e3faaad2b5705046bdb51f1681c4f1a0e0f1e195
Linguistics <thylinguistics@gmail.com>
2020-11-22 18:17:45 -0800

    adding planet comp's / refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
-import Planet from './Planet';
+import Mars from './planets/Mars';
 import LandsatEarth from './LandsatEarth';
 import '../styles/App.css';
 
@@ -14,7 +14,7 @@ const App = ()=>{
             <Header />
             <div className="container m-auto my-12">
             <Route path="/" exact component={Home} />
-            <Route path="/planet/:id" exact component={Planet} />
+            <Route path="/planet/mars" exact component={Mars} />
             <Route path="/landsat/earth" exact component={LandsatEarth} />
             </div>
         </Router>
---
+++ src/components/planets/Mars.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState, Fragment } from 'react';
+import axios from 'axios';
+
+
+const Mars = ({ match }) => {
+    const [planet, setPlanet] = useState(match.params.id);
+    const [marsPhotos, setMarsPhotos] = useState([]);
+    const [marsRover, setMarsRover] = useState({});
+
+    
+    useEffect(()=>{
+    console.log(planet);
+    }, [])
+
+
+    const marsRoverPics = async()=>{
+            const  KEY = 'edz1IInEZpnZubV09AMVZYN1746qxWQtvhMHZ72T';
+            const response = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=${KEY}`);
+        
+            setMarsPhotos(response.data.photos);
+            setMarsRover(response.data.photos[0].rover);
+            }
+    
+
+
+    const marsRoverButton = ()=>{
+        
+            return (
+                <div className="my-3">
+                 <button  
+                type="submit"
+                className= "bg-black hover:bg-blue-900 text-white font-bold py-2 px-4"
+                onClick={()=> marsRoverPics()}>
+                View mars photo's
+                </button>
+                </div>
+            )
+    }
+
+
+    const renderMarsRover = ()=>{
+        if(marsPhotos !== null){
+            return marsPhotos.map((photo)=>{
+                return (
+                    <Fragment key={photo.id}>
+                        <img src={`${photo.img_src}`} alt="planet mars" className="my-2 w-1/2 p-2 md:w-1/3" />
+                    </Fragment>
+                )
+            });
+        }     
+      };
+
+
+      const renderMarsRoverInfo = ()=>{
+        if(marsRover.name){
+            return (
+                <Fragment>
+                    <li>Rover Name: {marsRover.name}</li>
+                    <li>Launch Date: {marsRover.launch_date}</li>
+                    <li>Landing Date: {marsRover.landing_date}</li>
+                    <li>Status: {marsRover.status}</li>
+                </Fragment>
+            )
+        }     
+      };
+
+
+
+
+
+    return (
+        <div className="container mx-auto">
+            {planet}
+            {marsRoverButton()}
+            {renderMarsRoverInfo()}
+            <div className="flex flex-row flex-wrap justify-between my-2">
+                  {renderMarsRover()}
+            </div>
+        </div>
+    )
+}
+
+export default Mars
