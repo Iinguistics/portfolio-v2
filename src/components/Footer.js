57bbf048fe962d4b07e3873f44db9dd9364fc384
Linguistics <thylinguistics@gmail.com>
2020-11-30 17:05:18 -0800

    updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="flex flex-row justify-around h-20 bg-gray-900 items-center">
-            <div>TEst</div>
-            <div>TEst</div>
+        <div className="flex flex-row justify-around h-20 bg-gray-900 items-center text-white text-xl mt-4" >
+            <a href="/" >
+            <p>JG</p>
+            </a>
+            <a href="https://github.com/Iinguistics" target="_blank" rel="noopener noreferrer">
+            <img src={require('../assets/icons/github-icon.png')} alt="github"/> 
+            </a>
         </div>
     )
 }
