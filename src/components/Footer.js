82a5e2be815152cf45bfb45c7657fef495589f4b
Linguistics <thylinguistics@gmail.com>
2020-12-12 11:37:33 -0800

    added linkedin link to footer for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="flex flex-row justify-around h-20 bg-gray-900 items-center text-xl mt-20 font-extrabold">
-            <a href="/" >
+        <div className="flex flex-row justify-center md:justify-around h-20 bg-gray-900 items-center text-xl mt-20 font-extrabold">
+            <a href="/" className="invisible lg:visible">
             <span className="text-white">J</span><span className="text-black">G</span>
+            </a>
+            <a className="visible lg:invisible mr-10" href="https://www.linkedin.com/in/JamesGoytia" target="_blank" rel="noopener noreferrer">
+            <img id="linkedin-icon-footer" src={require('../assets/icons/linkedin-icon.png')} alt="linkedin"/> 
             </a>
             <a href="https://github.com/Iinguistics" target="_blank" rel="noopener noreferrer">
             <img src={require('../assets/icons/github-icon.png')} alt="github"/> 
